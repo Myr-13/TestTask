6f45aaf97219792bbed7907dae511bfc987f9b7c
Myr <hui@tebe.com>
2025-05-20 00:03:29 +0500

    Basic ORM layout

--- src/main.py
+++ src/main.py
@@ -1,30 +1,23 @@
 from fastapi import FastAPI
-import uvicorn
-import argparse
+import os
 import logging
 import contextlib
 
 from routers.auth import router
 
 from base.config import CONFIG
-from base.database import DATABASE
+from base.database import create_database
 
 
 @contextlib.asynccontextmanager
 async def lifespan(app: FastAPI):
 	logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-	parser = argparse.ArgumentParser(exit_on_error=False)
-	parser.add_argument("--config", "-c", type=str, help="Path to config file")
-	args = parser.parse_args()
-
-	CONFIG.load_config(args.config)
-	await DATABASE.open_connection(CONFIG["database_path"])
-	print(CONFIG["database_path"])
+	config = os.getenv("APP_CONFIG")
+	CONFIG.load_config(config)
+	create_database(CONFIG["database_path"])
 
 	yield
-
-	await DATABASE.close_connection()
 
 
 app = FastAPI(lifespan=lifespan)
